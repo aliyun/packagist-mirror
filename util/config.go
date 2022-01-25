16f6eaa88f3e9fcf0e482255b03db290893a2c9c
Jackson Tian <shyvo1987@gmail.com>
2022-01-25 12:20:27 +0800

    Add validateConfig method

--- util/config.go
+++ util/config.go
@@ -3,12 +3,13 @@ package util
 import (
 	"errors"
 	"fmt"
-	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
 	"runtime"
 	"strings"
 	"sync"
+
+	"gopkg.in/yaml.v2"
 )
 
 const (
@@ -63,7 +64,7 @@ func loadConfig() {
 	}
 }
 
-func getConf(conf *Config) error {
+func getConf(conf *Config) (err error) {
 	system := runtime.GOOS
 	path, _ := os.Getwd()
 	fmt.Println(path, strings.TrimRight(path, "/main"))
@@ -74,77 +75,97 @@ func getConf(conf *Config) error {
 	}
 	ymlFile, err := os.Open(path)
 	if err != nil {
-		return err
+		return
 	}
 	defer ymlFile.Close()
 	yamlContent, err := ioutil.ReadAll(ymlFile)
 	if err != nil {
-		return err
+		return
 	}
 	err = yaml.Unmarshal(yamlContent, conf)
 	if err != nil {
-		return err
+		return
 	}
 
+	err = validateConfig(conf)
+	return
+}
+
+func validateConfig(conf *Config) (err error) {
 	if conf.RedisAddr == "" {
 		err = errors.New("please set necessary environment variable: REDIS_ADDR")
+		return
 	}
 
 	if conf.RedisPassword == "" {
 		err = errors.New("please set necessary environment variable: REDIS_PASSWORD")
+		return
 	}
 
 	if conf.OSSAccessKeyID == "" {
 		err = errors.New("please set necessary environment variable: OSS_ACCESS_KEY_ID")
+		return
 	}
 
 	if conf.OSSAccessKeySecret == "" {
 		err = errors.New("please set necessary environment variable: OSS_ACCESS_KEY_SECRET")
+		return
 	}
 
 	if conf.OSSEndpoint == "" {
 		err = errors.New("please set necessary environment variable: OSS_ENDPOINT")
+		return
 	}
 
 	if conf.OSSBucket == "" {
 		err = errors.New("please set necessary environment variable: OSS_BUCKET")
+		return
 	}
 
 	if conf.GithubToken == "" {
 		err = errors.New("please set necessary environment variable: GITHUB_TOKEN")
+		return
 	}
 
 	if conf.MirrorUrl == "" {
 		err = errors.New("please set necessary environment variable: MIRROR_URL")
+		return
 	}
 
 	if conf.RepoUrl == "" {
 		err = errors.New("please set necessary environment variable: REPO_URL")
+		return
 	}
 
 	if conf.ApiUrl == "" {
 		err = errors.New("please set necessary environment variable: API_URL")
+		return
 	}
 
 	if conf.ProviderUrl == "" {
 		err = errors.New("please set necessary environment variable: PROVIDER_URL")
+		return
 	}
 
 	if conf.DistUrl == "" {
 		err = errors.New("please set necessary environment variable: DIST_URL")
+		return
 	}
 
 	if conf.BuildCache == "" {
 		err = errors.New("please set necessary environment variable: BUILD_CACHE")
+		return
 	}
 
 	if conf.UserAgent == "" {
 		err = errors.New("please set necessary environment variable: USER_AGENT")
+		return
 	}
 
 	if conf.ApiIterationInterval <= 0 {
 		err = errors.New("please set necessary environment variable: API_ITERATION_INTERVAL")
+		return
 	}
 
-	return err
+	return
 }
