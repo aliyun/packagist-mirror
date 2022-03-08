218c92da5119bc1a631fbf3f895eb0e58c9d8baf
Jackson Tian <shyvo1987@gmail.com>
2022-03-08 12:37:51 +0800

    fix coding style issues

--- util/config.go
+++ util/config.go
@@ -54,77 +54,77 @@ func getYamlContent(yamlPath string) (content []byte, err error) {
 
 func (config *Config) ValidateConfig() (err error) {
 	if config.RedisAddr == "" {
-		err = errors.New("Missing configuration: REDIS_ADDR")
+		err = errors.New("missing configuration: REDIS_ADDR")
 		return
 	}
 
 	if config.RedisPassword == "" {
-		err = errors.New("Missing configuration: REDIS_PASSWORD")
+		err = errors.New("missing configuration: REDIS_PASSWORD")
 		return
 	}
 
 	if config.OSSAccessKeyID == "" {
-		err = errors.New("Missing configuration: OSS_ACCESS_KEY_ID")
+		err = errors.New("missing configuration: OSS_ACCESS_KEY_ID")
 		return
 	}
 
 	if config.OSSAccessKeySecret == "" {
-		err = errors.New("Missing configuration: OSS_ACCESS_KEY_SECRET")
+		err = errors.New("missing configuration: OSS_ACCESS_KEY_SECRET")
 		return
 	}
 
 	if config.OSSEndpoint == "" {
-		err = errors.New("Missing configuration: OSS_ENDPOINT")
+		err = errors.New("missing configuration: OSS_ENDPOINT")
 		return
 	}
 
 	if config.OSSBucket == "" {
-		err = errors.New("Missing configuration: OSS_BUCKET")
+		err = errors.New("missing configuration: OSS_BUCKET")
 		return
 	}
 
 	if config.GithubToken == "" {
-		err = errors.New("Missing configuration: GITHUB_TOKEN")
+		err = errors.New("missing configuration: GITHUB_TOKEN")
 		return
 	}
 
 	if config.MirrorUrl == "" {
-		err = errors.New("Missing configuration: MIRROR_URL")
+		err = errors.New("missing configuration: MIRROR_URL")
 		return
 	}
 
 	if config.RepoUrl == "" {
-		err = errors.New("Missing configuration: REPO_URL")
+		err = errors.New("missing configuration: REPO_URL")
 		return
 	}
 
 	if config.ApiUrl == "" {
-		err = errors.New("Missing configuration: API_URL")
+		err = errors.New("missing configuration: API_URL")
 		return
 	}
 
 	if config.ProviderUrl == "" {
-		err = errors.New("Missing configuration: PROVIDER_URL")
+		err = errors.New("missing configuration: PROVIDER_URL")
 		return
 	}
 
 	if config.DistUrl == "" {
-		err = errors.New("Missing configuration: DIST_URL")
+		err = errors.New("missing configuration: DIST_URL")
 		return
 	}
 
 	if config.BuildCache == "" {
-		err = errors.New("Missing configuration: BUILD_CACHE")
+		err = errors.New("missing configuration: BUILD_CACHE")
 		return
 	}
 
 	if config.UserAgent == "" {
-		err = errors.New("Missing configuration: USER_AGENT")
+		err = errors.New("missing configuration: USER_AGENT")
 		return
 	}
 
 	if config.ApiIterationInterval <= 0 {
-		err = errors.New("Missing configuration: API_ITERATION_INTERVAL")
+		err = errors.New("missing configuration: API_ITERATION_INTERVAL")
 		return
 	}
 
